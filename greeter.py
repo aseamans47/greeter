e80d51c986aca3ceac2605a4f18b3f774b449cbf
aseamans47 <aseamans47@gmail.com>
2019-12-20 14:45:32 -0800

    Comment add

--- greeter.py
+++ greeter.py
@@ -1,12 +1,13 @@
 import pyttsx3
 import time
 
+
 currTime = time.localtime()
 
 engine = pyttsx3.init()
 engine.setProperty('voice', 'irish-gaeilge')
 
-
+#Time is 24 hour configuration with the assumption that 1700 is considered evening.
 if currTime.tm_hour < 12:
     engine.say("Good Morning ")
 elif (currTime.tm_hour >= 12) & (currTime.tm_hour < 17):
