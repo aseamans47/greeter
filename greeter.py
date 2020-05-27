0bbd64b0312dfc89251c4f07c14e7eb50b5ce0e7
aseamans47 <35788323+aseamans47@users.noreply.github.com>
2020-05-26 20:04:29 -0700

    Add files via upload

--- greeter.py
+++ greeter.py
@@ -1,20 +1,80 @@
 import pyttsx3
 import time
+import json
+import requests
 
 
-currTime = time.localtime()
-
+#Initial setup of voice engine.
 engine = pyttsx3.init()
 engine.setProperty('voice', 'irish-gaeilge')
 
-#Time is 24 hour configuration with the assumption that 1700 is considered evening.
-if currTime.tm_hour < 12:
-    engine.say("Good Morning ")
-elif (currTime.tm_hour >= 12) & (currTime.tm_hour < 17):
-    engine.say("Good Afternoon")
-else:
-    engine.say("Good Evening")
+#Checks for configuration file and returns a boolean value.
+def validateConfig():
+    
+    try:
+        config = json.load(open('config.json'))
+        return True
+    except IOError:
+        print('Configuration file not found\nCreating new Configuration file')
+        return False
 
 
-engine.runAndWait()
+#Prompts the user for configuration information and stores it in a JSON file
+def userConfig():
+    
+    userInfo = {
+        'name' : '',
+        'zip code' : ''}
+
+    userInfo['name'] = raw_input('Enter Your Name \n')
+    userInfo['zip code'] = raw_input('Enter your ZIP Code \n')
+
+    with open('config.json', 'w') as c:
+        userInfo["name"] = userInfo['name']
+        userInfo['zip code'] = userInfo['zip code']
+        json.dump(userInfo, c)
+
+
+#pulls time and gives proper greeting
+def greet(userName):
+
+    currTime = time.localtime()
+    
+    if currTime.tm_hour < 12:
+        engine.say("Good Morning {}".format(userName))
+    elif (currTime.tm_hour >= 12) & (currTime.tm_hour < 17):
+        engine.say("Good Afternoon {}".format(userName))
+    else:
+        engine.say("Good Evening {}".format(userName))
+    
+    engine.runAndWait()
+
+#pulls weather using zip code from config file.
+def getTemp(location):
+    weatherData = requests.get("http://api.openweathermap.org/data/2.5/weather?zip={},us&appid=16f2529eb6bf744dc55161010e203414&units=imperial".format(location))
+    currTemp = weatherData.json()['main']['temp']
+
+    engine.say("The temperature is {}".format(currTemp))
+
+    engine.runAndWait()
+
+
+def main():
+    
+    if validateConfig() == False:
+        userConfig()
+
+    with open('config.json', 'r') as c:
+        userData = json.load(c)
+        userName = userData['name']
+        zipCode = userData['zip code']
+ 
+    greet(userName)
+    getTemp(zipCode)
+
+
+main()
+
+
+
 
